aba10ee4eb50b2a7bb947a5c728fd9c8c7df307c
Moses Surumen <moses.susrumen@gmail.com>
2019-06-13 00:32:30 -0700

    initial commit

---
+++ 01-strings/1-concatenation-indexing-and-slicing.py
@@ -0,0 +1,42 @@
+# 1 - Concatenation, Indexing, and Slicing
+
+# Exercise 1
+# Display the number of letters in the string
+my_word = "antidisestablishmentarianism"
+
+#Solution
+print(len(my_word))
+
+
+# Exercise 2
+# Concatenate two strings together
+string_left = "bat"
+string_right = "man"
+
+#Solution
+print(string_left + string_right)
+
+
+# Exercise 3
+# Display two strings together, with a space in between
+string_one = "heebie"
+string_two = "jeebies"
+
+#Solution
+print(string_one, string_two)
+
+
+# Exercise 3
+# Display two strings together, with a space in between
+string_one = "heebie"
+string_two = "jeebies"
+
+#Solution
+print(string_one + " " + string_two)
+
+
+# Exercise 4
+# Use subscripting to display part of a string
+
+#Solution
+print("bazinga"[2:6])
---
+++ 03-conditional-logic/5-simulate-events-and-calculate-probabilities.py
@@ -0,0 +1,24 @@
+# 5 - Simulate Events and Calculate Probabilities
+
+
+from random import randint
+
+
+# Exercise 1
+# Write a function that simulatee the roll of a die.
+def roll():
+    """Return random integer between 1 and 6"""
+    return randint(1, 6)
+
+
+# Exercise 2
+# Simulate 10,000 rolls of a die and display the average number rolled.
+num_rolls = 10_000
+total = 0
+
+for trial in range(num_rolls):
+    total = total + roll()
+
+avg_roll = total / num_rolls
+
+print(f"The average result of {num_rolls} rolls is {avg_roll}")
